4f736b3e6da0cdf6991c28646cc34c21ffe5cc13
hiroakiito <pigaru.gooood.mar19@gmail.com>
2020-06-06 08:41:50 +0900

    ヘッダー大枠レイアウト作成

--- header.php
+++ header.php
@@ -16,7 +16,8 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
-
+	<link rel="SHORTCUT ICON" href="<?php bloginfo('template_directory'); ?>/images/favicon.ico" />
+  <link href="https://use.fontawesome.com/releases/v5.9.0/css/all.css" rel="stylesheet">
 	<?php wp_head(); ?>
 </head>
 
@@ -26,34 +27,36 @@
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'sample-theme' ); ?></a>
 
 	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$sample_theme_description = get_bloginfo( 'description', 'display' );
-			if ( $sample_theme_description || is_customize_preview() ) :
-				?>
+		<div class="site-branding container">
+				<div class="head-left">
+						<?php
+						// 現在のサイトに登録されているカスタムロゴを表示する。
+					the_custom_logo(); ?>
+						<?php
+					// サイトの説明が設定されていれば表示する
+					$sample_theme_description = get_bloginfo( 'description', 'display' );
+					if ( $sample_theme_description || is_customize_preview() ) :
+						?>
+				</div>
+
+				<!-- サイトの説明文 -->
 				<p class="site-description"><?php echo $sample_theme_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
 			<?php endif; ?>
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'sample-theme' ); ?></button>
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);
-			?>
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="true"><?php esc_html_e( 'Primary Menu', 'sample-theme' ); ?></button>
+				<?php
+				wp_nav_menu(
+					array(
+						'menu_class' => 'container',
+						'theme_location' => 'menu-1',
+						'menu_id'        => 'primary-menu',
+					)
+				);
+				?>
+			</ul>
 		</nav><!-- #site-navigation -->
+
+
 	</header><!-- #masthead -->

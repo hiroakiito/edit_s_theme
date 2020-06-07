54f3151d6800bbcecf806d80bfb6504503c84dfb
hiroakiito <pigaru.gooood.mar19@gmail.com>
2020-06-07 21:26:30 +0900

    sp版レイアウト配置大枠

--- footer.php
+++ footer.php
@@ -16,14 +16,16 @@
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'sample-theme' ) ); ?>">
 				<?php
 				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'sample-theme' ), 'WordPress' );
+				// printf( esc_html__( 'Proudly powered by %s', 'sample-theme' ), 'WordPress' );
 				?>
 			</a>
-			<span class="sep"> | </span>
-				<?php
+			<!-- <span class="sep"> | </span> -->
+				<p class="copy"><small><?php
 				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'sample-theme' ), 'sample-theme', '<a href="http://underscores.me/">Hiroaki Ito</a>' );
-				?>
+				// printf( esc_html__( 'Theme: %1$s by %2$s.', 'sample-theme' ), 'sample-theme', '<a href="http://underscores.me/">Hiroaki Ito</a>' );
+				printf( esc_html__( '&copy; 2020 A3Create inc.', 'sample-theme' ), 'sample-theme', '<a href="http://underscores.me/">Hiroaki Ito</a>' );
+				?></small>
+				</p>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
